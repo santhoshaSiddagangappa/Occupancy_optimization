42d5add11105f51a689bc9c04f2a9737733868dc
santhoshaSiddagangappa <39939685+santhoshaSiddagangappa@users.noreply.github.com>
2021-02-12 16:41:28 +0530

    initial commit.

---
+++ src/test/java/com/smarthost/challenge/CallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthost.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
